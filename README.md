2b3458f26823e344c4a65f57b390c2713ed7ec57
Rohit <48400770+rohitdasu@users.noreply.github.com>
2023-04-15 00:50:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Next.js Version 13 + Tailwind CSS
+
+Boilerplate code for [Next.js](https://nextjs.org/) with [TailwindCSS](https://tailwindcss.com/), [Prettier](https://prettier.io/) and [Husky](https://www.npmjs.com/package/husky).
+
+Created this repository as a template to create new projects without installing the supporting packages (tailwindCSS, Prettier etc.)
---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Title</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to <span className="text-blue-600">Next.js 13</span>
+        </h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
