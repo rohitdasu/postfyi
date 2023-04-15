ff5487906843c96d7d9dcfa6c486c0377bf4a80a
rohitdasu <dasurohit123@gmail.com>
2023-04-15 10:18:53 +0530

    feat: added landing page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IM+Fell+French+Canon+SC&family=Inter:wght@400;500;600;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter'],
+        canon: ['IM Fell French Canon SC'],
+      },
+    },
   },
   plugins: [],
-}
+};
